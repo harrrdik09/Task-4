6003e7932932bc69a586fabef82d74c46a1dd086
harrrdik09 <hardikprajapati8098@gmail.com>
2025-08-17 14:53:18 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String fn = "src/product.csv";
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(fn));
+            FileWriter fw = new FileWriter("src/Target.csv",true))
+
+        {
+
+            while ((line = br.readLine()) != null) {
+                // Split by comma
+                String[] data = line.split(",");
+
+                String productName = data[0];
+                double price = Double.parseDouble(data[1]);
+
+                // Print output
+               // System.out.println("Product: " + productName + " | Price: " + price);
+                if (price>1000)
+                {
+
+                        System.out.println(productName + price);
+                        fw.write(productName + " " + price +"\n");
+
+
+                    System.out.println("Data Come Succesfuly");
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+
+    }
+}
